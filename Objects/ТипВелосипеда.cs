b0dbd933c0ae87e21c26f9b83b910f9d4b546078
Lihtee <aproleev@ics.perm.ru>
2018-07-06 16:23:46 +0500

    Это надо удалить

--- Objects/ТипВелосипеда.cs
+++ Objects/ТипВелосипеда.cs
@@ -13,6 +13,8 @@ namespace IIS.Прокат_велосипедов_2
     using System;
     using System.Xml;
     using ICSSoft.STORMNET;
+    using ICSSoft.STORMNET.Business.Audit;
+    using ICSSoft.STORMNET.Business.Audit.Objects;
     
     
     // *** Start programmer edit section *** (Using statements)
@@ -29,6 +31,14 @@ namespace IIS.Прокат_велосипедов_2
     [AutoAltered()]
     [Caption("Тип велосипеда")]
     [AccessType(ICSSoft.STORMNET.AccessType.none)]
+    [View("AuditView", new string[] {
+            "Название as \'Название\'",
+            "ТекущаяСтоимость as \'Текущая стоимость\'",
+            "Актуально as \'Актуально\'",
+            "ДатаНачалаАктуальности as \'Дата начала актуальности\'",
+            "ДатаОкончанияАктуальности as \'Дата окончания актуальности\'"})]
+    [AssociatedDetailViewAttribute("AuditView", "ИсторияРасценок", "AuditView", true, "", "История расценок", true, new string[] {
+            ""})]
     [View("ТипВелосипедаE", new string[] {
             "Название as \'Название\'",
             "ТекущаяСтоимость as \'Текущая стоимость\'",
@@ -43,7 +53,7 @@ namespace IIS.Прокат_велосипедов_2
             "Актуально as \'Актуально\'",
             "ДатаНачалаАктуальности as \'Дата начала актуальности\'",
             "ДатаОкончанияАктуальности as \'Дата окончания актуальности\'"})]
-    public class ТипВелосипеда : ICSSoft.STORMNET.DataObject, IIS.Прокат_велосипедов_2.IАктуально
+    public class ТипВелосипеда : ICSSoft.STORMNET.DataObject, IIS.Прокат_велосипедов_2.IАктуально, IDataObjectWithAuditFields
     {
         
         private string fНазвание;
@@ -53,6 +63,14 @@ namespace IIS.Прокат_велосипедов_2
         private System.DateTime fДатаНачалаАктуальности;
         
         private ICSSoft.STORMNET.UserDataTypes.NullableDateTime fДатаОкончанияАктуальности;
+        
+        private System.Nullable<System.DateTime> fCreateTime;
+        
+        private string fCreator;
+        
+        private System.Nullable<System.DateTime> fEditTime;
+        
+        private string fEditor;
         
         private IIS.Прокат_велосипедов_2.DetailArrayOfИсторияРасценок fИсторияРасценок;
         
@@ -218,6 +236,132 @@ WHERE ИсторияРасценок.ТипВелосипеда = StormMainObjec
         }
         
         /// <summary>
+        /// Время создания объекта.
+        /// </summary>
+        // *** Start programmer edit section *** (ТипВелосипеда.CreateTime CustomAttributes)
+
+        // *** End programmer edit section *** (ТипВелосипеда.CreateTime CustomAttributes)
+        public virtual System.Nullable<System.DateTime> CreateTime
+        {
+            get
+            {
+                // *** Start programmer edit section *** (ТипВелосипеда.CreateTime Get start)
+
+                // *** End programmer edit section *** (ТипВелосипеда.CreateTime Get start)
+                System.Nullable<System.DateTime> result = this.fCreateTime;
+                // *** Start programmer edit section *** (ТипВелосипеда.CreateTime Get end)
+
+                // *** End programmer edit section *** (ТипВелосипеда.CreateTime Get end)
+                return result;
+            }
+            set
+            {
+                // *** Start programmer edit section *** (ТипВелосипеда.CreateTime Set start)
+
+                // *** End programmer edit section *** (ТипВелосипеда.CreateTime Set start)
+                this.fCreateTime = value;
+                // *** Start programmer edit section *** (ТипВелосипеда.CreateTime Set end)
+
+                // *** End programmer edit section *** (ТипВелосипеда.CreateTime Set end)
+            }
+        }
+        
+        /// <summary>
+        /// Создатель объекта.
+        /// </summary>
+        // *** Start programmer edit section *** (ТипВелосипеда.Creator CustomAttributes)
+
+        // *** End programmer edit section *** (ТипВелосипеда.Creator CustomAttributes)
+        [StrLen(255)]
+        public virtual string Creator
+        {
+            get
+            {
+                // *** Start programmer edit section *** (ТипВелосипеда.Creator Get start)
+
+                // *** End programmer edit section *** (ТипВелосипеда.Creator Get start)
+                string result = this.fCreator;
+                // *** Start programmer edit section *** (ТипВелосипеда.Creator Get end)
+
+                // *** End programmer edit section *** (ТипВелосипеда.Creator Get end)
+                return result;
+            }
+            set
+            {
+                // *** Start programmer edit section *** (ТипВелосипеда.Creator Set start)
+
+                // *** End programmer edit section *** (ТипВелосипеда.Creator Set start)
+                this.fCreator = value;
+                // *** Start programmer edit section *** (ТипВелосипеда.Creator Set end)
+
+                // *** End programmer edit section *** (ТипВелосипеда.Creator Set end)
+            }
+        }
+        
+        /// <summary>
+        /// Время последнего редактирования объекта.
+        /// </summary>
+        // *** Start programmer edit section *** (ТипВелосипеда.EditTime CustomAttributes)
+
+        // *** End programmer edit section *** (ТипВелосипеда.EditTime CustomAttributes)
+        public virtual System.Nullable<System.DateTime> EditTime
+        {
+            get
+            {
+                // *** Start programmer edit section *** (ТипВелосипеда.EditTime Get start)
+
+                // *** End programmer edit section *** (ТипВелосипеда.EditTime Get start)
+                System.Nullable<System.DateTime> result = this.fEditTime;
+                // *** Start programmer edit section *** (ТипВелосипеда.EditTime Get end)
+
+                // *** End programmer edit section *** (ТипВелосипеда.EditTime Get end)
+                return result;
+            }
+            set
+            {
+                // *** Start programmer edit section *** (ТипВелосипеда.EditTime Set start)
+
+                // *** End programmer edit section *** (ТипВелосипеда.EditTime Set start)
+                this.fEditTime = value;
+                // *** Start programmer edit section *** (ТипВелосипеда.EditTime Set end)
+
+                // *** End programmer edit section *** (ТипВелосипеда.EditTime Set end)
+            }
+        }
+        
+        /// <summary>
+        /// Последний редактор объекта.
+        /// </summary>
+        // *** Start programmer edit section *** (ТипВелосипеда.Editor CustomAttributes)
+
+        // *** End programmer edit section *** (ТипВелосипеда.Editor CustomAttributes)
+        [StrLen(255)]
+        public virtual string Editor
+        {
+            get
+            {
+                // *** Start programmer edit section *** (ТипВелосипеда.Editor Get start)
+
+                // *** End programmer edit section *** (ТипВелосипеда.Editor Get start)
+                string result = this.fEditor;
+                // *** Start programmer edit section *** (ТипВелосипеда.Editor Get end)
+
+                // *** End programmer edit section *** (ТипВелосипеда.Editor Get end)
+                return result;
+            }
+            set
+            {
+                // *** Start programmer edit section *** (ТипВелосипеда.Editor Set start)
+
+                // *** End programmer edit section *** (ТипВелосипеда.Editor Set start)
+                this.fEditor = value;
+                // *** Start programmer edit section *** (ТипВелосипеда.Editor Set end)
+
+                // *** End programmer edit section *** (ТипВелосипеда.Editor Set end)
+            }
+        }
+        
+        /// <summary>
         /// Тип велосипеда.
         /// </summary>
         // *** Start programmer edit section *** (ТипВелосипеда.ИсторияРасценок CustomAttributes)
@@ -259,6 +403,17 @@ WHERE ИсторияРасценок.ТипВелосипеда = StormMainObjec
         {
             
             /// <summary>
+            /// "AuditView" view.
+            /// </summary>
+            public static ICSSoft.STORMNET.View AuditView
+            {
+                get
+                {
+                    return ICSSoft.STORMNET.Information.GetView("AuditView", typeof(IIS.Прокат_велосипедов_2.ТипВелосипеда));
+                }
+            }
+            
+            /// <summary>
             /// "ТипВелосипедаE" view.
             /// </summary>
             public static ICSSoft.STORMNET.View ТипВелосипедаE
@@ -279,6 +434,98 @@ WHERE ИсторияРасценок.ТипВелосипеда = StormMainObjec
                     return ICSSoft.STORMNET.Information.GetView("ТипВелосипедаL", typeof(IIS.Прокат_велосипедов_2.ТипВелосипеда));
                 }
             }
+        }
+        
+        /// <summary>
+        /// Audit class settings.
+        /// </summary>
+        public class AuditSettings
+        {
+            
+            /// <summary>
+            /// Включён ли аудит для класса.
+            /// </summary>
+            public static bool AuditEnabled = true;
+            
+            /// <summary>
+            /// Использовать имя представления для аудита по умолчанию.
+            /// </summary>
+            public static bool UseDefaultView = false;
+            
+            /// <summary>
+            /// Включён ли аудит операции чтения.
+            /// </summary>
+            public static bool SelectAudit = true;
+            
+            /// <summary>
+            /// Имя представления для аудирования операции чтения.
+            /// </summary>
+            public static string SelectAuditViewName = "AuditView";
+            
+            /// <summary>
+            /// Включён ли аудит операции создания.
+            /// </summary>
+            public static bool InsertAudit = true;
+            
+            /// <summary>
+            /// Имя представления для аудирования операции создания.
+            /// </summary>
+            public static string InsertAuditViewName = "AuditView";
+            
+            /// <summary>
+            /// Включён ли аудит операции изменения.
+            /// </summary>
+            public static bool UpdateAudit = true;
+            
+            /// <summary>
+            /// Имя представления для аудирования операции изменения.
+            /// </summary>
+            public static string UpdateAuditViewName = "AuditView";
+            
+            /// <summary>
+            /// Включён ли аудит операции удаления.
+            /// </summary>
+            public static bool DeleteAudit = true;
+            
+            /// <summary>
+            /// Имя представления для аудирования операции удаления.
+            /// </summary>
+            public static string DeleteAuditViewName = "AuditView";
+            
+            /// <summary>
+            /// Путь к форме просмотра результатов аудита.
+            /// </summary>
+            public static string FormUrl = "";
+            
+            /// <summary>
+            /// Режим записи данных аудита (синхронный или асинхронный).
+            /// </summary>
+            public static ICSSoft.STORMNET.Business.Audit.Objects.tWriteMode WriteMode = ICSSoft.STORMNET.Business.Audit.Objects.tWriteMode.Synchronous;
+            
+            /// <summary>
+            /// Максимальная длина сохраняемого значения поля (если 0, то строка обрезаться не будет).
+            /// </summary>
+            public static int PrunningLength = 0;
+            
+            /// <summary>
+            /// Показывать ли пользователям в изменениях первичные ключи.
+            /// </summary>
+            public static bool ShowPrimaryKey = false;
+            
+            /// <summary>
+            /// Сохранять ли старое значение.
+            /// </summary>
+            public static bool KeepOldValue = true;
+            
+            /// <summary>
+            /// Сжимать ли сохраняемые значения.
+            /// </summary>
+            public static bool Compress = false;
+            
+            /// <summary>
+            /// Сохранять ли все значения атрибутов, а не только изменяемые.
+            /// </summary>
+            public static bool KeepAllValues = false;
         }
     }
 }
